310531122d41f544911e195267b29bd4cc992491
alicemorenac <98626322+alicemorenac@users.noreply.github.com>
2022-04-15 21:39:23 -0300

    Update function.cpp
    retangulo 1 ok

--- ponto_em_retangulo_1/src/function.cpp
+++ ponto_em_retangulo_1/src/function.cpp
@@ -1,13 +1,31 @@
 #include "function.h"
 
-/*! 
- * Verifica se um ponto está dentro de um retângulo.
- */
 
-location_t pt_in_rect( const Ponto &IE, const Ponto &SD, const Ponto &P )
+location_t pt_in_rect(const Ponto& IE, const Ponto& SD, const Ponto& P)
 {
-    // TODO: Coloque aqui seu código.
+    if ((P).x > (IE).x && (P).x < (SD).x && (P).y >(IE).y && (P).y < (SD).y)
+    {
+        return location_t::INSIDE;
+    }
+    else if ((P).x == (IE).x && (P).y >= (IE).y && (P).y <= (SD).y && (P).x < (SD).x)
+    {
+        return location_t::BORDER;
+    }
+    else if ((P).x == (SD).x && (P).y >= (IE).y && (P).y <= (SD).y && (P).x > (IE).x)
+    {
+        return location_t::BORDER;
+    }
+    else if ((P).y == (IE).y && (P).x >= (IE).x && (P).x <= (SD).x && (P).y < (SD).y)
+    {
+        return location_t::BORDER;
+    }
+    else if ((P).y == (SD).y && (P).x >= (IE).x && (P).x <= (SD).x && (P).y > (IE).y)
+    {
+        return location_t::BORDER;
+    }
+    else
+    {
+        return location_t::OUTSIDE;
+    }
 
-    // TODO: Substitua o retorno conforme desejar. Isso é apenas um STUB, usado apenas para compilar.
-    return location_t::OUTSIDE;
 }
