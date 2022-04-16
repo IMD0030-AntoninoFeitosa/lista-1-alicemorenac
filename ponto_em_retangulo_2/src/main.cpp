1ab93f0339423977087b37edd6bd9d3a6f9fecd9
alicemorenac <98626322+alicemorenac@users.noreply.github.com>
2022-04-15 21:42:56 -0300

    Update main.cpp
    retangulo 2 ok

--- ponto_em_retangulo_2/src/main.cpp
+++ ponto_em_retangulo_2/src/main.cpp
@@ -1,9 +1,3 @@
-/*!
- * @brief Implementação do Ponto em Retângulo V2.
- * @author selan
- * @data June, 6th 2021
- */
-
 #include <iostream>
 using std::cout;
 using std::cin;
@@ -14,14 +8,53 @@ using std::max;
 
 #include "function.h"
 
-//=== Funções
+void verificar_pontos(Ponto* P1, Ponto* P2)
+{
 
-// Coloque aqui qualquer função auxiliar que desejar.
+    Ponto temporario;
 
+    if (!((P1->x < P2->x) || (P1->x == P2->x && P1->y < P2->y) || (P1->y == P2->y && P1->x < P2->x)))
+    {
+        temporario = *P1;
+        *P1 = *P2;
+        *P2 = temporario;
+    }
+}
 
 int main(void)
 {
-    // TODO: Adicione aqui seu código.
+    Ponto P;
+    Ponto IE;
+    Ponto SD;
+
+    while (cin >> std::ws >> IE.x >> std::ws >> IE.y >> std::ws >> SD.x >> std::ws >> SD.y >> std::ws >> P.x >> std::ws >> P.y)
+    {
+        if (IE.y == SD.y && IE.x == SD.x)
+        {
+            std::cout << "invalid" << std::endl;
+        }
+        else
+        {
+            verificar_pontos(&IE, &SD);
+
+            location_t result = pt_in_rect(IE, SD, P);
+
+            if (result == 0)
+            {
+                std::cout << "inside" << std::endl;
+            }
+            else if (result == 1)
+            {
+                std::cout << "border" << std::endl;
+            }
+            else
+            {
+                std::cout << "outside" << std::endl;
+            }
+        }
+    }
 
     return 0;
+
 }
+
