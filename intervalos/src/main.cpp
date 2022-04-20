946fa5e2a0cf9ddbfb77138198c86120dc55ceca
Alice Rocha <98626322+alicemorenac@users.noreply.github.com>
2022-04-20 20:50:54 +0000

    quantidade de variáveis reduzidas

--- intervalos/src/main.cpp
+++ intervalos/src/main.cpp
@@ -1,62 +1,52 @@
 #include <iostream>
-#include <vector>
 using std::cout;
 using std::cin;
 using std::endl;
 #include <iomanip>
 using std::setprecision;
 
-
 int main(void)
 {
-	int x = 0;
+	int x;
 	int nDados = 0;
-	std::vector <int> inter = { 0,0,0,0,0 };
-	std:: vector <double> porcent = { 0,0,0,0,0};
+	int inter1 = 0;
+  int inter2 = 0;
+  int inter3 = 0;
+  int inter4 = 0;
+  int interOut = 0;
+  	
+	while( cin >> std::ws >> x) {
+    nDados++;
+    
+    if(x >= 0 && x < 25)
+    {
+      inter1++;
+    } else if(x >= 25 && x < 50)
+    {
+      inter2++;
+    } else if(x >= 50 && x < 75)
+    {
+      inter3++;
+    } else if(x >= 75 && x < 100)
+    {
+      inter4++;
+    } else 
+    {
+      interOut++;
+    }
+  }
 
-	while (std::cin >> std::ws >> x)
-	{
+	cout << std::setprecision(4) << (inter1*100.0)/ (nDados) << endl;
 
-		if (x >= 0 && x < 25)
-		{
-			inter[0]++;
-		}
-		else if (x >= 25 && x < 50)
-		{
-			inter[1]++;
-		}
-		else if (x >= 50 && x < 75)
-		{
-			inter[2]++;
-		}
-		else if (x >= 75 && x < 100)
-		{
-			inter[3]++;
-		}
-		else
-		{
-			inter[4]++;
-		}
+	cout << std::setprecision(4) << (inter2*100.0)/(nDados) << endl;
 
-		nDados = nDados++;
-	}
+	cout << std::setprecision(4) << (inter3*100.0)/(nDados) << endl;
 
-	porcent[0] = (inter[0] / static_cast<double> (nDados)) * 100;
-	porcent[1] = (inter[1] / static_cast<double> (nDados)) * 100;
-	porcent[2] = (inter[2] / static_cast<double> (nDados)) * 100;
-	porcent[3] = (inter[3] / static_cast<double> (nDados)) * 100;
-	porcent[4] = (inter[4] / static_cast<double> (nDados)) * 100;
+	cout << std::setprecision(4) << (inter4*100.0)/(nDados) << endl;
 
-	std::cout << std::setprecision(4) << porcent[0] << std::endl;
-
-	std::cout << std::setprecision(4) << porcent[1] << std::endl;
-
-	std::cout << std::setprecision(4) << porcent[2] << std::endl;
-
-	std::cout << std::setprecision(4) << porcent[3] << std::endl;
-
-	std::cout << std::setprecision(4) << porcent[4] << std::endl;
+	cout << std::setprecision(4) << (interOut*100.0)/(nDados) << endl;
 
 	return 0;
 
 }
+
