180b66c201c84577fbae966b492363333fb55556
Alice Rocha <98626322+alicemorenac@users.noreply.github.com>
2022-04-21 19:00:26 +0000

    alterações finais

--- soma_vizinhos/src/main.cpp
+++ soma_vizinhos/src/main.cpp
@@ -1,36 +1,28 @@
 #include <iostream>
-#include <cstdlib>
 using std::cout;
 using std::cin;
 using std::endl;
 
-
 int main( void )
 {
-	int n = 0;
-	int m = 0;
-	int soma = 0;
-
-	cin >> m;
-	cin >> n;
-
-
-	if (n == 0)
-	{
-		soma = m;
-	}
-	else if (n > 0)
-	{
-		for (int i = m; i < m + n; i++)
-			soma = soma + i;
-	}
-	else if (n < 0)
-	{
-		for (int i = m; i > m - abs(n); i--)
-			soma = soma + i;
-	}
-
-	cout << soma << endl;
-
-	return 0;
+  int m, n, soma;
+  while(cin >> std::ws >> m >> n){
+    soma = 0;
+    if(n > 0){
+      for(int i=0;i<n;i++){
+        soma += m;
+        m++;
+      }
+    } else if(n < 0){
+      for(int i=0;i<-n;i++){
+        soma += m;
+        m--;
+      }
+    } else {
+      soma = m;
+    }
+    cout << soma << endl;
+  }
+  return 0;
 }
+
